5d5f27cf346a16ab33a40ef7fcd7a415cdd4a587
Mugera <mugera1540@gmail.com>
2025-12-18 16:52:55 +0530

    Our Values Section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Navbar from '../Navbar/Navbar'
 import Hero from '../Hero/Hero'
 import Category from '../Category/Category'
+import Values from '../Values/Values'
+import Products from '../Products/Products'
 
 const Home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
-      <Category/>
+      <Category />
+      <Values />
+      < Products/>
 
     </div>
   )
